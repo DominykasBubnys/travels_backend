7c858ca906d6b4da776c282d4b78cbe3e954607f
Roberta Aleksandraitytė <bubnys.it@gmail.com>
2022-05-26 11:34:55 +0300

    fixing authentication bugs

--- app.js
+++ app.js
@@ -1,33 +1,37 @@
-const express = require("express");
-const bodyParser = require('body-parser');
-const mongoose = require("mongoose");
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 // Routes
-const PlaceRoute = require("./routes/place-routes");
-const UserRoute = require("./routes/user-routes");
+const PlaceRoute = require('./routes/place-routes')
+const UserRoute = require('./routes/user-routes')
 
-const app = express();
+const app = express()
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set('view engine', 'ejs')
+app.set('views', 'views')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  )
+  res.setHeader('Access-Control-Allow-methods', 'GET,POST,PATCH,DELETE')
 
-app.use((req,res,next) => {
-    res.setHeader("Access-Control-Allow-Origin","*");
-    res.setHeader("Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.setHeader("Access-Control-Allow-methods","GET,POST,PATCH,DELETE");
-
-    next();
+  next()
 })
 
-app.use(UserRoute);
+app.use(UserRoute)
 
-app.use(PlaceRoute);
+app.use(PlaceRoute)
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.4afhm.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
-.then(app.listen(process.env.PORT || 5000))
-.catch(err => console.log("connecting mongoose returns an error: ", err));
+mongoose
+  .connect(
+    `mongodb+srv://dombub1:dombub1@cluster0.4afhm.mongodb.net/travels?retryWrites=true&w=majority`
+  )
+  .then(app.listen(process.env.PORT || 5000))
+  .catch((err) => console.log('connecting mongoose returns an error: ', err))
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,139 +1,123 @@
-const User = require("../models/user");
-const mongoose = require('mongoose');
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+const User = require('../models/user')
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
+const GetUsers = async (req, res, next) => {
+  try {
+    const data = await User.find({}, '-password')
 
-const GetUsers = async(req,res,next) => {
-
-  try{
-    const data = await User.find({}, "-password");
-
-    res.json({users: data.map(user => user.toObject({getters: true}))});
-
-  }catch(err){
-    res.status(401).send({message: "Failed to fetch users. Please try again later"});
-    throw new Error(err);
+    res.json({ users: data.map((user) => user.toObject({ getters: true })) })
+  } catch (err) {
+    return res
+      .status(401)
+      .send({ message: 'Failed to fetch users. Please try again later' })
   }
-
 }
 
-const GetUserById = async (req,res,next) => {
-
-  const userId = req.params.uid;
+const GetUserById = async (req, res, next) => {
+  const userId = req.params.uid
 
   try {
-    const user = await User.findById(userId);
+    const user = await User.findById(userId)
 
-    if(!user)throw new Error("Cannot find user with provided ID")
+    if (!user) throw new Error('Cannot find user with provided ID')
 
-    res.status(200).send({user: user});
+    res.status(200).send({ user: user })
   } catch (err) {
-    res.status(501).send({message: "Cannot load user with provided id"});
-    console.log("error'as: ", err);
+    return res
+      .status(501)
+      .send({ message: 'Cannot load user with provided id' })
   }
-
 }
-
 
 // < CRUD OPERATIONS >
 
+const POST_signUp = async (req, res, next) => {
+  const { name, email, password } = req.body
 
-const POST_signUp = async(req,res,next) => {
-
-    const {name, email, password} = req.body;
-
-    let hashedPassword;
-
-    try {
-      hashedPassword = await bcrypt.hash(password, 12);
-    } catch (err) {
-      console.log(err);
-      res.status(501).send({message: "Failed to bcrypt ypur password :("});
-    }
-
-    const newUser = new User({
-      _id: new mongoose.Types.ObjectId(),
-      name: name,
-      email: email,
-      password: hashedPassword,
-      image: "image url2",
-      places: []
-    })
-
-    try {
-      const egsistingUser = await User.find({email: email})
-
-      if(egsistingUser.length > 0){
-        const errText = "The email address you have entered is already associated with another account.";
-        res.status(501).send({message: errText})
-        throw new Error(errText)
-      }
-      
-      await newUser.save();
-
-      let token = jwt.sign(
-        {userId: egsistingUser._id, email: egsistingUser.email},
-        "supersecret_dont_share",
-        {expiresIn: "1h"}
-      );
-
-      console.log("Registracija sekminga")
-
-      res.status(200).send({user: newUser, token: token})
-      
-    } catch (err) {
-      res.status(501).send({
-        message: "Registration fails! Please try again"
-      })
-    }
-
-}
-
-const Login = async (req,res,next) => {
-  const {email,password} = req.body;
-
-  let indentifiedUser;
-
-  try{
-    indentifiedUser = await User.findOne({email: email})
-
-    if(!indentifiedUser){
-      res.status(501).send({message: "Cannot find user with provided email! Please try again..."});
-    }
-
-  }catch(err){
-    res.status(501).send({message: "Cannot find user with provided email! Please try again..."});
-    console.log(err);
-  }
-
-  let isValidPassword = false;
+  let hashedPassword
 
   try {
-    isValidPassword = await bcrypt.compare(password, indentifiedUser.password);
-
-    if(!isValidPassword){
-      res.status(500).send({message: "Wrong password! Please try again..."});
-    }
-
-    let token = jwt.sign(
-      {userId: indentifiedUser._id, email: indentifiedUser.email},
-      "supersecret_dont_share",
-      {expiresIn: "1h"}
-    );
-
-    console.log("Prisijungimas sekmingas")
-
-    res.status(200).send({user: indentifiedUser, token: token})
-
+    hashedPassword = await bcrypt.hash(password, 12)
   } catch (err) {
-    res.status(501).send({message: "failed to login..."});
+    console.log(err)
+    return res
+      .status(501)
+      .send({ message: 'Failed to bcrypt ypur password :(' })
   }
 
-  
+  const newUser = new User({
+    _id: new mongoose.Types.ObjectId(),
+    name: name,
+    email: email,
+    password: hashedPassword,
+    image: 'image url2',
+    places: []
+  })
+
+  try {
+    const egsistingUser = await User.find({ email: email })
+
+    if (egsistingUser.length > 0) {
+      const errText =
+        'The email address you have entered is already associated with another account.'
+      return res.status(501).send({ message: errText })
+    }
+
+    await newUser.save()
+
+    let token = jwt.sign(
+      { userId: egsistingUser._id, email: egsistingUser.email },
+      'supersecret_dont_share',
+      { expiresIn: '1h' }
+    )
+
+    return res.status(200).send({ user: newUser, token: token })
+  } catch (err) {
+    return res.status(501).send({
+      message: 'Registration fails! Please try again'
+    })
+  }
 }
 
-exports.GetUsers = GetUsers;
-exports.GetUserById = GetUserById;
-exports.POST_signUp = POST_signUp;
-exports.Login = Login;
+const Login = async (req, res, next) => {
+  const { email, password } = req.body
+
+  let indentifiedUser
+
+  try {
+    indentifiedUser = await User.findOne({ email: email })
+
+    if (!indentifiedUser) {
+      return res.status(501).send({
+        message: 'Cannot find user with provided email! Please try again...'
+      })
+    }
+  } catch (err) {
+    return res.status(501).send({
+      message: 'Cannot find user with provided email! Please try again...'
+    })
+  }
+
+  let isValidPassword = false
+
+  try {
+    isValidPassword = await bcrypt.compare(password, indentifiedUser.password)
+
+    if (!isValidPassword) {
+      return res
+        .status(500)
+        .send({ message: 'Wrong password! Please try again...' })
+    }
+
+    return res.status(200).send({ user: indentifiedUser, token: token })
+  } catch (err) {
+    return res.status(501).send({ message: 'failed to login...' })
+  }
+}
+
+exports.GetUsers = GetUsers
+exports.GetUserById = GetUserById
+exports.POST_signUp = POST_signUp
+exports.Login = Login
